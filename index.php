8605bc3d76972147af698ecd235fd6afb3b5724c
Lucas <lukalds@gmail.com>
2020-05-30 13:56:11 -0300

    Alterações Lucas 2

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     $nome   = isset($_POST['nome'])? $_POST['nome']:'';
     $checks = isset($_POST['check'])? $_POST['check']:'';
 
+    $validaCpf = '';
+    $error = '';
+
     if($cpf) $validaCpf = validaCpf($cpf); //VERIFICANDO SE O CPF É VÁLIDO
 
     if($validaCpf === false) {
@@ -39,76 +42,6 @@
 ?>
 
 <html>
-    <head>
-        <title>Enquete 2020</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="libs/bootstrap-4.4.1-dist/css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="css/styles.css">
-
-    </head>
-    <body>
-
-            <nav class="navbar navbar-dark bg-dark">
-                 <span class="navbar-text">Eleiçoes 2020</span>
-            </nav>
-
-                    <hr>
-  <form name="usuario" method="POST">
-            <!-- new-form -->
-  <div class="card user-info">
-  <div class="card-body ">
-  <div class="form-group">
-        <p>Para participar da votação, por favor insira seu  nome e seu CPF</p>
-         <label for="nomeInput">Nome </label>
-           <input type="text" id="nome" name="nome" class="form-control"  placeholder="Insira seu nome"  aria-describedby="emailHelp">
-    </div>
-     <div class="form-group">
-       <label for="cpfUsuario">CPF</label>
-      <input type="text" id="cpf" name="cpf" class="form-control"  placeholder="Insira seu CPF">
-  </div>
-    <div class="txt-center">
-          <button type="submit" class="btn btn-primary "  id="acao" name="acao" data-toggle="modal" data-target="#modalExemplo">Enviar</button>
-    </div>
-   </div>
-   </div>
-        <hr>
-
-         
-            <div class="main-cards">
-                <?php 
-                    foreach ($candidatos as $can){ //INICIO FOREACH
-                        $idCandidato      = $can['id'];
-                        $nomeCandidato    = $can['nome'];
-                        $partidoCandidato = $can['partido'];
-                ?>
-                <div class="card">
-                 <div class="card-body">
-                    <h5 class="card-title"  value="<?php echo $nomeCandidato ?>">
-                         <?php    echo $nomeCandidato ?>
-                    </h5>
-                    <img src="assets/unavailable-image.jpg" class="card-img-top" alt="...">
-                    <p class="card-text" value="<?php echo $partidoCandidato ?>"><b>Partido:&nbsp;</b>
-                        <?php    echo $partidoCandidato ?>
-                    </p>
-                    <p class="card-text"><b>periodo de mandato:&nbsp;</b>06-2010</p>
-                    <!-- <div class="custom-control custom-checkbox"> -->
-                <input type="checkbox" name="check[]" id="check" value="<?php echo $idCandidato ?>"> <!-- INSERINDO INPUT COM ID DE CADA CANDIDATO -->
-                    <!-- <label class="custom-control-label" for="check">selecionar candidato</label> -->
-                <!-- </div> -->
-                
-                </div>
-                </div>
-                <!-- <input type="checkbox" name="check[]" id="check" value="<?php echo $idCandidato ?>"> INSERINDO INPUT COM ID DE CADA CANDIDATO -->
-               <!-- Default checked -->
-                
-                <br><hr>
-                <?php
-                    } //FIM FOREACH
-                ?>
-            </div>
-        </form>
-    </body>
-</html><html>
     <head>
         <title>Enquete 2020</title>
         <meta charset="utf-8">
